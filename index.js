862bd5233ffe15c7f4fdd5c1910acb6fb67e7f99
Densuki <DarkShadow3315@gmail.com>
2018-06-08 22:39:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  //let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 });
 //==============================================================
