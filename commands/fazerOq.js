1fbd73df13fd1adad8f870b5e112281e804cd83f
Densuki <DarkShadow3315@gmail.com>
2018-06-08 22:25:35 -0300

    Update fazerOq.js

--- commands/fazerOq.js
+++ commands/fazerOq.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
 
     .addField("http://prntscr.com/jhraie");
 
-      return message.channel.send(fazerEmbed);
+    message.channel.send(fazerEmbed);
 
 }
 
