f7ba3e60f454f8c8a272236aa426a43ac0533a67
Densuki <DarkShadow3315@gmail.com>
2018-09-17 13:56:39 -0300

    nada demais

--- commands/informacao.js
+++ commands/informacao.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      let bicon = bot.user.displayAvatarURL;
+
       let botembed = new Discord.RichEmbed()
       .setDescription("**Informações para sugestões**")
       .setColor("#5F04B4") //ROXO
