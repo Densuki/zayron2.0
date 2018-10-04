c02eb06d81e3f6b5e627cf830413a83dcc84b3cb
Densuki <DarkShadow3315@gmail.com>
2018-10-03 22:10:00 -0300

    a

--- commands/say.js
+++ commands/say.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-//      if(!message.member.hasPermission("ADMINISTRATOR")) return;
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+//      if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
