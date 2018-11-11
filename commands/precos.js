26a5bd2c18b71264435019080a95db2201317242
Densuki <DarkShadow3315@gmail.com>
2018-11-11 10:02:50 -0200

    Update commands/precos.js

--- commands/precos.js
+++ commands/precos.js
@@ -15,10 +15,10 @@ module.exports.run = async (bot, message, args) => {
 //client.users.get("name", "USERNAMEHERE").id;
 //<%@ID%>
 //@name
-metion = message.author.send;
+//metion = message.author.send;
 
     let precosEmbed = new Discord.RichEmbed()
-    .setDescription(metion "**Olá!** *Use os comandos em seu devido local.* \n\n **digite um dos comandos abaixo para entrar na tabela de preços que deseja:** \n\n :rocket: **EpicMC Network** \n\n **| vanilla | applied | tinkers | draconic | ic2 | thermal | factory | ender |**")
+    .setDescription("**Olá!** *Use os comandos em seu devido local.* \n\n **digite um dos comandos abaixo para entrar na tabela de preços que deseja:** \n\n :rocket: **EpicMC Network** \n\n **| vanilla | applied | tinkers | draconic | ic2 | thermal | factory | ender |**")
     .setColor("#5F04B4") //ROXO
     .addField(":exclamation: **Esteja ciente do PREFIX do BOT!** **O PREFIX deste artigo é o padrão.**", "**:warning:  Avisos**")
     //.addField("**:warning:  Avisos**")
