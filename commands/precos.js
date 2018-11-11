f35534bb05945c4e5257c590000fc4c5363f108b
Densuki <DarkShadow3315@gmail.com>
2018-11-11 09:12:16 -0200

    Update commands/precos.js

--- commands/precos.js
+++ commands/precos.js
@@ -9,15 +9,16 @@ module.exports.run = async (bot, message, args) => {
       message.reply("```Aqui será dado uma amostra do que tem na economia do servidor```");
       return;
     }
+//metion = message.mentions.users.first();
 
     let precosEmbed = new Discord.RichEmbed()
-    .setDescription(message.guild.member, "**Olá!**, *Use os comandos em seu devido local.* \n\n **digite um dos comandos abaixo para entrar na tabela de preços que deseja:** \n\n :rocket: **EpicMC Network** \n\n **?vanilla | ?applied | ?tinkers | ?draconic | ?ic2 | ?thermal | ?factory | ?ender |**")
+    .setDescription(message.mentions.users "**Olá!** {user}, *Use os comandos em seu devido local.* \n\n **digite um dos comandos abaixo para entrar na tabela de preços que deseja:** \n\n :rocket: **EpicMC Network** \n\n **?vanilla | ?applied | ?tinkers | ?draconic | ?ic2 | ?thermal | ?factory | ?ender |**")
     .setColor("#5F04B4") //ROXO
     .addField(":exclamation: **Esteja ciente do PREFIX do BOT!** **O PREFIX deste artigo é o padrão.**", "**:warning:  Avisos**")
     //.addField("**:warning:  Avisos**")
-    .addField("**Essa é a economia da Netowrk. Caso você queira vender com preços maiores é por sua conta.** **Porém, qualquer preço que esteja a baixo do valor que foi proposto em alguma desta lista, o usuário será punido!**", "```**Essa lista sempre será atualizada, fique de olho.**```")
+    .addField("**Essa é a economia da Netowrk. Caso você queira vender com preços maiores é por sua conta.** **Porém, qualquer preço que esteja a baixo do valor que foi proposto em alguma desta lista, o usuário será punido!**", "**Essa lista sempre será atualizada, fique de olho.**")
     //.addField("**Essa lista sempre será atualizada, fique de olho.**")
-    .addField(":calendar_spiral: **As listas serão atualizadas.**", "```**FIQUE ATENTO(A)!**```");
+    .addField(":calendar_spiral: **As listas serão atualizadas.**", "**FIQUE ATENTO(A)!**");
     //.addField("**FIQUE ATENTO(A)!**");
 
     message.channel.send(precosEmbed);
