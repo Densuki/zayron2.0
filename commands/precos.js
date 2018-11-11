825b989d33ccc27510f1bcdc7b4d4454f6826a65
Densuki <DarkShadow3315@gmail.com>
2018-11-11 08:53:07 -0200

    Teste

---
+++ commands/precos.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  //%report @ned Esta é a razão (Reação?)
+
+
+    //if (args[0] == "help"){
+    //  message.reply("use: %sugestao seu nome + sua sugestão\n\n exemplo: `%sugestao @zayron para ser um STAFF!`");
+    //  return;
+    //}
+
+    let precosEmbed = new Discord.RichEmbed()
+    .setDescription("**Olá!**, *Use os comandos em seu devido local.* \n\n **digite um dos comandos abaixo para entrar na tabela de preços que deseja:** \n\n :rocket: **EpicMC Network** \n\n **?vanilla | ?applied | ?tinkers | ?draconic | ?ic2 | ?thermal | ?factory | ?ender |**")
+    .setColor("#5F04B4") //ROXO
+    .addField(":exclamation: **Esteja ciente do PREFIX do BOT!** **O PREFIX deste artigo é o padrão.**")
+    .addField("**:warning:  Avisos**")
+    .addField("**Essa é a economia da Netowrk. Caso você queira vender com preços maiores é por sua conta.** **Porém, qualquer preço que esteja a baixo do valor que foi proposto em alguma desta lista, o usuário será punido!**")
+    .addField("**Essa lista sempre será atualizada, fique de olho.**")
+    .addField(":calendar_spiral: **As listas serão atualizadas.**")
+    .addField("**FIQUE ATENTO(A)!**");
+    
+    //message.channel.send(reportEmbed);
+
+    //let reportschannel = message.guild.channels.find(`name`, "canal-de-sugestões");
+    //Nome do canal (acho que ele cria o canal 🤔)
+    //if(!reportschannel) return message.channel.send("não foi possível encontrar o canal de sugestões.");
+
+
+    //message.delete().catch(O_o=>{});
+    //reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name:"preços"
+}
