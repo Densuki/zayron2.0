7e8c572f5c4d6af722b77f5243109eb8ffebf0ec
Densuki <DarkShadow3315@gmail.com>
2018-11-19 13:46:09 -0200

    Update oi.js

--- commands/oi.js
+++ commands/oi.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 //import hljs from 'highlight.js/lib/highlight';
 //import javascript from 'highlight.js/lib/languages/javascript';
 //hljs.registerLanguage('javascript', javascript);
-import 'highlight.js/styles/github.css';
+//import 'highlight.js/styles/github.css';
 
 module.exports.run = async (bot, message, args) => {
 
