40fb16f158ad1ad99d2c3683f5b94ce880b602e8
Densuki <DarkShadow3315@gmail.com>
2018-11-19 13:27:56 -0200

    Update oi.js

--- commands/oi.js
+++ commands/oi.js
@@ -2,19 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-
-BUILDDIR      = _build
-EXTRAS       ?= $(BUILDDIR)/extras
-
-.PHONY: main clean
-
-main:
-	@echo "Building main facility..."
-	build_main $(BUILDDIR)
-
-clean:
-	rm -rf $(BUILDDIR)/*
-
 //==============================================================
 //MODELO DE MENSAGENS
 //==============================================================
